d312f17c4e00d6f02cbd5da774b0aa9374bda332
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-13 19:52:08 +0800

    Update fakeDB.js

--- src/common/fakeDB.js
+++ src/common/fakeDB.js
@@ -29,13 +29,13 @@ export const POSTS = [
         }
     },
     {
-        username: 'Miranda West',
+        username: 'John Finnegan',
         timeago: 'Last online 3 mins ago',
         group: 'Food',
         description: 'Check out my new photoshoot!',
         responses: 2,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg',
+        avatar: 'https://randomuser.me/api/portraits/med/men/42.jpg',
         website: {
             image: 'https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg',
             title: 'Pow Wow Cafe',
@@ -44,104 +44,135 @@ export const POSTS = [
         }
     },
     {
-        username: 'Miranda West',
+        username: 'Kuroski',
         timeago: '12min ago',
         responses: 5,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg',
+        avatar: 'https://randomuser.me/api/portraits/med/men/2.jpg',
         notice: 'I \'m soooo proud of my Toronto Raptors!!! \n\nLet\'s go Raptors, Let\'s go!',
     },
     {
-        username: 'Name',
+        username: 'Kenis Ian',
         timeago: '12min ago',
         description: 'Check Carley\'s quiz results: Chief!',
         responses: 34,
         fav: false,
-        avatar: 'https://answersafrica.com/wp-content/uploads/2014/03/1522669710-3227-Brenda-Wairimu.png'
+        avatar: 'https://randomuser.me/api/portraits/med/men/12.jpg'
     },
     {
-        username: 'Miranda West',
+        username: 'Teni West',
         timeago: 'Last online 3 mins ago',
         group: 'Food',
-        description: 'Hey anyone been here? What do you think of this place as a first date?',
+        description: 'Hey anyone been here? What do you think of this place? ',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/men/1.jpg',
+        cateogry: {
+            slug: 'Jangle',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROPD3TmsLI1Jd7s1fQ9s_zs0RxM03I_1pbo_OaKlob4Vtwc7kX&s',
+            content: 'Bring in the 2019 holidays with Popcorn Bucket.'
+        }
     },
     {
-        username: 'Miranda West',
+        username: 'Jonana Kebro',
         timeago: '12min ago',
         description: 'Check out my new photoshoot!',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/men/5.jpg',
+        website: {
+            image: 'https://i0.wp.com/disneydiary.com/wp-content/uploads/2019/11/Mickey%E2%80%99s-Very-Merry-Christmas-Party-2019-Disney-Pins-1.jpg',
+            title: 'Disney World',
+            content: 'Mickey\'s Very Merry Christmas Party 2019 pins to be released',
+            url: 'http://www.disneydiary.com/'
+        }
     },
     {
-        username: 'Name',
+        username: 'James Cook',
         timeago: '12min ago',
         group: '',
         description: 'Check Carley\'s quiz results: Chief!',
         responses: 34,
         fav: true,
-        avatar: 'https://answersafrica.com/wp-content/uploads/2014/03/1522669710-3227-Brenda-Wairimu.png'
+        avatar: 'https://randomuser.me/api/portraits/med/women/1.jpg',
+        notice: 'Aladdin & Jasmine Singing Doll Aladdin 2019 \n\nDisney Store Japan - VeryGoods.JP'
     },
     {
-        username: 'Miranda West',
+        username: 'Suski Natalia',
         timeago: 'Last online 3 mins ago',
         group: 'Food',
         description: 'Check out my new photoshoot!',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/women/2.jpg',
+        photogroup: [
+            'https://ss-images.catscdn.vn/w500/2018/06/01/2924529/in1.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_Z5Yd2GW8ZLXgRDCRfiZqU6bO6krMA0IScD4Sj_ncauJWAd0-&s',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKuDcq7M5sfqIrVUnxnEk1RSe4hG9_OwgcEjjEZ5srA0bOdmJ2&s',
+        ]
     },
     {
-        username: 'Miranda West',
+        username: 'Jenny Akasha',
         timeago: '12min ago',
         description: 'Check out my new photoshoot!',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/women/3.jpg'
     },
     {
-        username: 'Name',
+        username: 'Tanvir Kumar',
         timeago: '12min ago',
         description: 'Check Carley\'s quiz results: Chief!',
         responses: 34,
         fav: true,
-        avatar: 'https://answersafrica.com/wp-content/uploads/2014/03/1522669710-3227-Brenda-Wairimu.png'
+        avatar: 'https://randomuser.me/api/portraits/med/women/4.jpg',
+        notice: 'Official\n\nPC Calendar Wallpaper|Tokyo Disney Resort'
     },
     {
-        username: 'Miranda West',
+        username: 'Ono Sabroa',
         timeago: 'Last online 3 mins ago',
         group: 'GYM',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg',
-        photo: 'https://www.moshimoshi-nippon.jp/wp/wp-content/uploads/2019/01/6cf82c835feb55b80a01b0e068122364.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/women/5.jpg',
+        photo: 'https://aws-tiqets-cdn.imgix.net/images/content/dea079c12bd94347a67d729bea5ab052.jpg?auto=format&fit=crop&ixlib=python-1.1.2&q=25&s=c2fc9f2bf9ff261a80c599d8811c033d&w=400&h=320&dpr=2.625'
     },
     {
-        username: 'Miranda West',
+        username: 'Anastasha',
         timeago: '12min ago',
         description: 'Check out my new photoshoot!',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/men/36.jpg',
+        cateogry: {
+            slug: 'Micky Mouse',
+            image: 'https://www.disney.co.jp/content/disney/jp/music/news/20190206/_jcr_content/par/navi_vertical_child/navi_vertical_child_par/rss_article/mainImage1.img.jpg/1549332783425.jpg',
+            content: 'Happiest micky mouse'
+        }
     },
     {
-        username: 'Name',
+        username: 'Natalia',
         timeago: '12min ago',
         description: 'Check Carley\'s quiz results: Chief!',
         responses: 34,
         fav: true,
-        avatar: 'https://answersafrica.com/wp-content/uploads/2014/03/1522669710-3227-Brenda-Wairimu.png'
+        avatar: 'https://randomuser.me/api/portraits/med/women/7.jpg',
+        photogroup: [
+            'https://media1.popsugar-assets.com/files/thumbor/yEtL1dtvXYv2kZSkBfuVlCvJte8/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/04/10/734/n/24155406/tmp_ix8PSy_874f59a8bd791af7_disney_villains.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbLy13diqBfYiE3fSliBqPtuFd8h50W_7MNO48DWiwDXxlozl1&s',
+            'https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2017/10/DisneysideHades.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjgjNsQxiJXE3_MrmkF-wJQPKXAnQP2VdCnFSMr3M2rafKelGK&s'
+        ]
     },
     {
-        username: 'Miranda West',
+        username: 'Misael Washa',
         timeago: 'Last online 3 mins ago',
         group: 'Sports',
         description: 'Check out my new photoshoot!',
         responses: 34,
         fav: true,
-        avatar: 'https://i.pinimg.com/originals/6b/d2/20/6bd220687b3744171973406b81e4b334.jpg'
+        avatar: 'https://randomuser.me/api/portraits/med/women/8.jpg',
+        photo: 'https://cdn7.wdwnt.com/wp-content/uploads/2018/11/iasw-2-768x512.jpg'
+
     },
 ]
